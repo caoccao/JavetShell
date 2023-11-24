a1977bcfddadcd3cf08cc067c2ab3a075683c12d
caoccao <caoccao@users.noreply.github.com>
2023-11-24 09:36:16 +0800

    🐞 fix: Fix gradle script for referencing local aar

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.caoccao.javet:javet-android:3.0.1")
-//    implementation(fileTree(dir: 'libs', includes: ['*.aar']))
+//    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
